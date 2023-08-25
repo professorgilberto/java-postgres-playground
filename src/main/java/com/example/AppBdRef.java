f56017f15fe67f72ebceb18040a744e66bd4da65
professorgilberto <professor.gilberto@gmail.com>
2023-08-25 17:13:58 +0000

    v3

--- src/main/java/com/example/AppBdRef.java
+++ src/main/java/com/example/AppBdRef.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -10,15 +11,43 @@ public class AppBdRef {
         new AppBdRef();
     }
     public AppBdRef(){
-        try {
-            var conn = DriverManager.getConnection("jdbc:sqlite:banco.db");
-            exibeMSG("conexao ok");
+        try (var conn = getConnection();){
+            exibeMSG("conexao com o banco estabelecida com sucesso");
+
+            criaTabela(conn);
+            Produtos produto1=new Produtos();
+            Produtos produto2=new Produtos();
+            Produtos produto3=new Produtos();
+            Produtos produto4=new Produtos();
+
+            produto1.setId(1);
+            produto1.setMarca("marca1");
+            produto1.setNome("nome1");
+            produto1.setValor(12.2);
+            
+            produto2.setId(2);
+            produto2.setMarca("marca2");
+            produto2.setNome("nome2");
+            produto2.setValor(22.2);
+
+            produto3.setId(3);
+            produto3.setMarca("marca3");
+            produto3.setNome("nome3");
+            produto3.setValor(32.2);
+
+            produto4.setId(4);
+            produto4.setMarca("marca4");
+            produto4.setValor(42.2);
+
+            insereProduto(conn,produto1);
+            insereProduto(conn,produto2);
+            insereProduto(conn,produto3);            
+            insereProduto(conn,produto4);
+
             var statement = conn.createStatement();
-            statement.executeUpdate("DROP TABLE IF EXISTS Produtos;");
-            statement.executeUpdate("CREATE TABLE Produtos (id INTEGER PRIMARY KEY AUTOINCREMENT, nome STRING, Marca STRING, Valor Numeric);");
-            statement.executeUpdate("INSERT INTO Produtos (NOME,MARCA,VALOR) VALUES('Blevers1', 'Marca1',12.3);");
-            statement.executeUpdate("INSERT INTO Produtos (NOME,MARCA,VALOR) VALUES('Blevers2', 'Marca2',22.3);");
-            statement.executeUpdate("INSERT INTO Produtos (NOME,MARCA,VALOR) VALUES('Blevers3', 'Marca3',33.3);");
+            //statement.executeUpdate("INSERT INTO Produtos (NOME,MARCA,VALOR) VALUES('Blevers1', 'Marca1',12.3);");
+            //statement.executeUpdate("INSERT INTO Produtos (NOME,MARCA,VALOR) VALUES('Blevers2', 'Marca2',22.3);");
+            //statement.executeUpdate("INSERT INTO Produtos (NOME,MARCA,VALOR) VALUES('Blevers3', 'Marca3',33.3);");
             exibeMSG("Antes Update");
             exibeBD(statement);
             statement.executeUpdate("UPDATE Produtos SET nome = 'BleversUPD' WHERE id=1;");
@@ -37,17 +66,32 @@ public class AppBdRef {
             System.err.println("Erro na conexão" + e.getMessage());
         }
 
+    }
+
+    private void insereProduto(Connection conn, Produtos produto) {
+
+        String sql = "Insert into Produtos (Nome,Marca,valor) values (?,?,?)";
+        try (var statement=conn.prepareStatement(sql);) {
+            statement.setString(1,produto.getNome());
+            statement.setString(2,produto.getMarca());
+            statement.setDouble(3,produto.getValor());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            System.err.println("Erro na inserção "+e.getMessage());
+        }
+      }
 
 
 
+
+    private static Connection getConnection() throws SQLException  {
+        return (DriverManager.getConnection("jdbc:sqlite:banco.db"));   
     }
 
 
 
 
-
-
-    static void exibeBD(final Statement statement){
+    private static void exibeBD(final Statement statement){
     
     try {
         final ResultSet rs = statement.executeQuery("SELECT * FROM Produtos");
@@ -63,13 +107,23 @@ public class AppBdRef {
         System.err.println("Erro na conexão" + e.getMessage());
     }
     }
-    static void exibeMSG(String MSG){
+    private static void exibeMSG(String MSG){
             System.out.println();   
             System.out.println(MSG);
             System.out.println();
         }
 
+    private static void criaTabela(Connection conn){
 
+            try {
+                var statement1=conn.createStatement();
+                statement1.executeUpdate("DROP TABLE  IF EXISTS PRODUTOS;");
+                statement1.executeUpdate("CREATE TABLE PRODUTOS ( ID INTEGER PRIMARY KEY AUTOINCREMENT,NOME STRING,MARCA STRING ,VALOR DECIMAL );");
+                exibeMSG("Tabela criada com sucesso!");
+            } catch (Exception e) {
+                System.err.println("Erro na criação da tabela "+ e.getMessage());
+            }
+        }
 
 
 }
---
+++ src/main/java/com/example/Produtos.java
@@ -0,0 +1,38 @@
+package com.example;
+
+public class Produtos {
+
+    private int id;
+    private String Nome;
+    private String Marca;
+    private Double Valor;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return Nome;
+    }
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+    public String getMarca() {
+        return Marca;
+    }
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+    public Double getValor() {
+        return Valor;
+    }
+    public void setValor(Double valor) {
+        Valor = valor;
+    }
+
+
+
+
+}
